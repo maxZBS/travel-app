42954ebeb497d46e7373004c864506422fcfefa5
Max Shushval <8555234@gmail.com>
2022-02-23 11:40:45 +0300

    Feat(schema): Add place schema

--- pages/api/places/index.ts
+++ pages/api/places/index.ts
@@ -6,7 +6,7 @@ export const places: IPlace[] = [
 		_id: 'wefwe23wdf',
 		slug: 'tokyo',
 		description:
-			'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,',
+			'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters',
 		imagePath: 'http://localhost:3000/images/places/japan.jpg',
 		location: { country: 'Japan', city: 'Kyoto' },
 		rating: 5,
--- travelapp/schemas/schema.js
+++ travelapp/schemas/schema.js
@@ -6,24 +6,20 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import place from './place'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    post,
-    author,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-  ]),
+	// We name our schema
+	name: 'default',
+	// Then proceed to concatenate our document type
+	// to the ones provided by any plugins that are installed
+	types: schemaTypes.concat([
+		// The following are document types which will appear
+		// in the studio.
+		place,
+		// When added to this list, object types can be used as
+		// { type: 'typename' } in other document schemas
+		blockContent,
+	]),
 })
