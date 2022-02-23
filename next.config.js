25374abb4aca881eba87483e7d9e71a73bd3c5a2
Max Shushval <8555234@gmail.com>
2022-02-23 11:08:43 +0300

    Feat(sanity): Integrate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const STUDIO_REWRITE = {
+	source: '/studio/:path*',
+	destination:
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:3333/studio/:path*'
+			: '/travelapp/index.html',
+}
+
 module.exports = {
 	reactStrictMode: true,
 	env: {
 		APP_URL: 'http://localhost:3000',
 	},
+	rewrites: () => [STUDIO_REWRITE],
 }
